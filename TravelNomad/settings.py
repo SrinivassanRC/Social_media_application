86664ef8ef5639d6930ce94f5be495decc4f931e
Srinivassan RC <69319014+SrinivassanRC@users.noreply.github.com>
2022-12-04 17:06:09 -0500

    Update settings.py

--- TravelNomad/settings.py
+++ TravelNomad/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,7 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR , 'staticfiles')
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
